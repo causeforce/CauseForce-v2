431b83e85d61e6c38b7f4244fdddab8dc11fdf99
Christian Solis <csolis@CF-MLT-02.local>
2017-10-18 15:18:37 -0700

    commit initial files

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'causeforce_v2');
+define('DB_NAME', 'cf2_database');
 
 /** MySQL database username */
 define('DB_USER', 'root');
