e10f27ecc7ec450c22d9a5a970c90ada2d4f48c9
Christian Solis <csolis@CF-MLT-02.local>
2017-10-26 18:58:20 -0700

    new home styles

--- wp-content/themes/cf_v2/assets/scripts/main.js
+++ wp-content/themes/cf_v2/assets/scripts/main.js
@@ -57,7 +57,7 @@
       init: function() {
         // JavaScript to be fired on the home page
         $(".gray-row").waypoint(function(){
-//            $('.gray-row').fadeToggle();
+        // $('.gray-row').fadeToggle();
             $('.square').addClass('show');
             $('#graph').addClass('show');
             }, { 
@@ -328,7 +328,27 @@
 
         
       }
+    },
+    'newhome': {
+        init: function() {
+
+
+
+      },
+      finalize: function() {        
+        var figure = $(".video").hover( hoverVideo, hideVideo );
+
+        function hoverVideo(e) {  
+            $('video', this).get(0).play(); 
+        }
+
+        function hideVideo(e) {
+            $('video', this).get(0).pause(); 
+        }
+
+      }
     }
+
 
   };
 
